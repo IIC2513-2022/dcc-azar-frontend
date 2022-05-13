5191dd06a495dcb3fa981a1479c95d0501afc352
Vicente Reyes <vicerc@uc.cl>
2022-05-13 14:04:48 -0400

    feat(react): react intro for capsula

---
+++ react-app/src/routes/user.jsx
@@ -0,0 +1,61 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+async function deleteUser(userId) {
+  const config = {
+    method: "delete",
+    url: `${process.env.REACT_APP_API_URL}/users/${userId}`,
+    headers: {},
+  };
+
+  return await axios(config)
+    .then(function () {
+      return true;
+    })
+    .catch(function () {
+      return false;
+    });
+}
+
+export default function User() {
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
+  const params = useParams();
+  const config = {
+    method: "get",
+    url: `${process.env.REACT_APP_API_URL}/${parseInt(params.userId, 10)}`,
+    headers: {},
+  };
+
+  useEffect(() => {
+    axios(config)
+      .then(function (response) {
+        setUser(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [params.userId]);
+
+  return (
+    <main style={{ padding: "1rem" }}>
+      <h2>
+        {user.firstName} {user.lastName}
+      </h2>
+      <p>Username: {user.username}</p>
+      <p>Age: {user.age}</p>
+      <p>
+        <button
+          onClick={async () => {
+            if (await deleteUser(user.id)) {
+              navigate("/users", { state: { deleted: user.id } });
+            }
+          }}
+        >
+          Delete
+        </button>
+      </p>
+    </main>
+  );
+}
---
+++ react-app/src/routes/users.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
+import axios from "axios";
+
+export default function Users() {
+  const [users, setUsers] = useState([]);
+  const location = useLocation();
+  const config = {
+    method: "get",
+    url: `${process.env.REACT_APP_API_URL}/users`,
+    headers: {},
+  };
+  useEffect(() => {
+    axios(config)
+      .then(function (response) {
+        setUsers(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [location.state]);
+  console.log();
+
+  return (
+    <div style={{ display: "flex" }}>
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem",
+        }}
+      >
+        {users.map((user) => (
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                margin: "1rem 0",
+                color: isActive ? "red" : "",
+              };
+            }}
+            to={`/users/${user.id}`}
+            key={user.id}
+          >
+            {user.username}
+          </NavLink>
+        ))}
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
